707974355802bb76a94efeb7e6037d70376568a9
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-09-01 15:52:36 -0700

    correct the order of the bounds check

--- uci_bootcamp_2021/examples/while_loops.py
+++ uci_bootcamp_2021/examples/while_loops.py
@@ -1,12 +1,8 @@
 """
+uci_bootcamp_2021/examples/while_loops.py
 
 Demonstrate basic while loop usage
-
 """
-
-
-def main():
-    ...
 
 
 def get_valid_input(
@@ -18,16 +14,24 @@ def get_valid_input(
 
     # loop while the user hasn't given us a suitable value.
     while not valid:
+        # get the user's untrusted input.
         unvalidated_input = input(prompt)
         # check if the input is numeric.
         if unvalidated_input.isnumeric():
             # if its numeric, we can safely interpret it as an integer
             result = int(unvalidated_input)
             # the we can check the bounds
-            valid = valid_maximum >= result >= valid_minimum
+            valid = valid_minimum <= result <= valid_maximum
         if not valid:
             print("invalid input, please try again!")
     return result
+
+
+def main():
+    valid_input = get_valid_input(
+        "Enter a number between 15 and 20", valid_minimum=15, valid_maximum=20
+    )
+    print(valid_input)
 
 
 # entry-point guard, since this example does blocking actions.
